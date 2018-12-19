599e224afd8748007e934b330603059038351eee
anita <anita.qolami1@gmail.com>
2018-12-19 18:47:07 +0330

    make laravel mix

---
+++ resources/js/admin.js
@@ -0,0 +1,7 @@
+require('./files/bootstrap-select.min');
+require('./files/bootstrap.min');
+require('./files/sweetalert.min');
+require('./files/jquery.min');
+require('./files/leaflet');
+require('./files/leaflet-src.esm');
+require('./files/leaflet-src');
