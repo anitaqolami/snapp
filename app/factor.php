451fa6ad9a26df9f74cd20c8500b95f2eef8ad23
anita <anita.qolami1@gmail.com>
2018-12-19 19:31:47 +0330

    add model & migrate map

--- app/factor.php
+++ app/factor.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class factor extends Model
 {
-    //
+    protected $fillable = ['shop_id','user_id','service_id','code_discount','price','service_discount','date','accept','status'];
 }
